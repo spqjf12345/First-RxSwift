65549150c616b7e4c4432f8f0d18dc3fbec4a7ca
jouureee <jouureee@gmail.com>
2021-11-20 15:11:17 +0900

    search text filter

---
+++ First-RxSwift/Controlllers/SearchCityViewController.swift
@@ -0,0 +1,51 @@
+//
+//  SearchCityViewController.swift
+//  First-RxSwift
+//
+//  Created by JoSoJeong on 2021/11/20.
+//
+
+import UIKit
+import RxCocoa
+import RxSwift
+
+class SearchCityViewController: UIViewController {
+    var shownCities = [String]() // Data source for UITableView
+    let allCities = ["New York", "London", "Oslo", "Warsaw", "Berlin", "Praga"]
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    let disposeBag = DisposeBag()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+        setUpSearchBar()
+    }
+    
+    func setUpSearchBar(){
+    self.searchBar.rx.text.orEmpty .debounce(RxTimeInterval.microseconds(5), scheduler: MainScheduler.instance) //0.5초 기다림
+        .distinctUntilChanged() // 같은 아이템을 받지 않는기능
+        .subscribe(onNext: { t in
+            self.shownCities = self.allCities.filter{ $0.hasPrefix(t) }
+            self.tableView.reloadData()
+        }).disposed(by: disposeBag)
+
+    
+    }
+    
+
+}
+
+extension SearchCityViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return shownCities.count
+    }
+     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cityPrototypeCell", for: indexPath)
+        cell.textLabel?.text = shownCities[indexPath.row]
+        
+        return cell
+    }
+}
