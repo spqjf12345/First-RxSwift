77dae31ade7f0883b33f7fafb271732fbda1b39a
jouureee <jouureee@gmail.com>
2022-03-02 18:06:59 +0900

    Add Colors file

--- First-RxSwift/Views/Main/Controller/MainViewController.swift
+++ First-RxSwift/Views/Main/Controller/MainViewController.swift
@@ -21,91 +21,90 @@ class MainViewController: UIViewController, Stepper {
 
     var btnLists : [UIButton] = []
         
-        var currentIndex : Int = 0 {
-            didSet{
-                changeBtnColor()
+    var currentIndex : Int = 0 {
+        didSet{
+            changeBtnColor()
+        }
+    }
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setBtnList()
+//            self.navigationController?.setNavigationBarHidden(true, animated: true)
+    }
+    
+    func setBtnList(){
+        allVC.tintColor = .orange
+        btnLists.append(allVC)
+        btnLists.append(textVC)
+        btnLists.append(linkVC)
+        btnLists.append(giftVC)
+        btnLists.append(calendarVC)
+        btnLists.append(profileVC)
+    }
+    
+    func changeBtnColor(){
+        
+        for (index, element) in btnLists.enumerated(){
+            
+            if index == currentIndex {
+                element.setTitleColor(#colorLiteral(red: 0.5568627451, green: 0.6392156863, blue: 0.8, alpha: 1), for: .normal)
             }
-        }
-        
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            setBtnList()
-            self.navigationController?.setNavigationBarHidden(true, animated: true)
-        }
-        
-        func setBtnList(){
-            allVC.tintColor = .orange
-            btnLists.append(allVC)
-            btnLists.append(textVC)
-            btnLists.append(linkVC)
-            btnLists.append(giftVC)
-            btnLists.append(calendarVC)
-            btnLists.append(profileVC)
-            
-        }
-        
-        func changeBtnColor(){
-            
-            for (index, element) in btnLists.enumerated(){
-                
-                if index == currentIndex {
-                    element.setTitleColor(#colorLiteral(red: 0.5568627451, green: 0.6392156863, blue: 0.8, alpha: 1), for: .normal)
-                }
-                else{
-                    element.setTitleColor(#colorLiteral(red: 0.09519775957, green: 0.1197544411, blue: 0.2188102901, alpha: 1), for: .normal)
-                }
-                
+            else{
+                element.setTitleColor(#colorLiteral(red: 0.09519775957, green: 0.1197544411, blue: 0.2188102901, alpha: 1), for: .normal)
             }
             
         }
         
-        
-        var pageViewController : PageViewController!
-        
-        override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-           
-            if segue.identifier == "pageViewController" {
-                
-                guard let vc = segue.destination as? PageViewController else {
-                    print("return")
-                    return}
-                pageViewController = vc
-                
-                pageViewController.completeHandler = { (result) in
-                    self.currentIndex = result
-                }
-                
+    }
+    
+    
+    var pageViewController : PageViewController!
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+       
+        if segue.identifier == "pageViewController" {
+            
+            guard let vc = segue.destination as? PageViewController else {
+                print("return")
+                return}
+            pageViewController = vc
+            
+            pageViewController.completeHandler = { (result) in
+                self.currentIndex = result
             }
             
         }
         
+    }
     
-        @IBAction func allVC(_ sender: Any) {
-            pageViewController.setViewcontrollersFromIndex(index: 0)
-        }
-    
-        @IBAction func textVC(_ sender: Any) {
-            pageViewController.setViewcontrollersFromIndex(index: 1)
-        }
-    
-        @IBAction func linkVC(_ sender: Any) {
-            pageViewController.setViewcontrollersFromIndex(index: 2)
-        }
-    
-        @IBAction func giftVC(_ sender: Any) {
-            pageViewController.setViewcontrollersFromIndex(index: 3)
-        }
 
-        @IBAction func calendarVC(_ sender: Any) {
-            pageViewController.setViewcontrollersFromIndex(index: 4)
-        }
+    @IBAction func allVC(_ sender: Any) {
+        pageViewController.setViewcontrollersFromIndex(index: 0)
+    }
 
-        @IBAction func profileVC(_ sender: Any) {
-            let profileVC = UIStoryboard(name: "Profile", bundle: nil).instantiateViewController(identifier: "ProfileViewController") as? ProfileViewController
-            self.navigationController?.setNavigationBarHidden(false, animated: true)
-            self.navigationController?.pushViewController(profileVC!, animated: true)
-           
-        }
+    @IBAction func textVC(_ sender: Any) {
+        pageViewController.setViewcontrollersFromIndex(index: 1)
+    }
+
+    @IBAction func linkVC(_ sender: Any) {
+        pageViewController.setViewcontrollersFromIndex(index: 2)
+    }
+
+    @IBAction func giftVC(_ sender: Any) {
+        pageViewController.setViewcontrollersFromIndex(index: 3)
+    }
+
+    @IBAction func calendarVC(_ sender: Any) {
+        pageViewController.setViewcontrollersFromIndex(index: 4)
+    }
+
+    @IBAction func profileVC(_ sender: Any) {
+        let profileVC = UIStoryboard(name: "Profile", bundle: nil).instantiateViewController(identifier: "ProfileViewController") as? ProfileViewController
+        self.navigationController?.setNavigationBarHidden(false, animated: true)
+        self.navigationController?.pushViewController(profileVC!, animated: true)
+       
+    }
     
     
 
