1912627b7909bd5a040dc4a04000b1e14db55729
jouureee <jouureee@gmail.com>
2022-03-06 16:07:08 +0900

    Update allBoxViewmodel input, ouput

--- First-RxSwift/ViewModels/AllBoxViewModel.swift
+++ First-RxSwift/ViewModels/AllBoxViewModel.swift
@@ -11,18 +11,35 @@ import RxCocoa
 import PhotosUI
 
 class AllBoxViewModel {
-    var folders = BehaviorSubject<[Folder]>(value: [])
-    var selectedFolders: Folder
-    var filteredFolders = BehaviorSubject<[Folder]>(value: [])
     var disposeBag = DisposeBag()
 
     init(selected: Folder){
         self.selectedFolders = selected
         getFolders()
     }
+    
+    struct Input {
+        let searchTextField: Observable<Void>
+        let refreshEvent: Observable<Void>
+        let floatingButtonTap: Observable<Void>
+        let folderCellTap: Observable<Int> // folderId
+        let chageFolderNameTap: Observable<Void>
+        let changeFolderImageTap: Observable<Void>
+        let deleteFolder: Observable<Void>
+        let sortingButtonTap: Observable<Void>
+    }
   
+    struct Output {
+        var folderCount: Observable<String> //개의 폴더
+        var sortingText: Observable<String> //이름순, 생성순, 최신순
+        var folders = BehaviorSubject<[Folder]>(value: [])
+        var selectedFolders: Folder
+        var filteredFolders = BehaviorSubject<[Folder]>(value: [])
+    }
+    
     func getFolders(){
         //getfolder
+        
     }
     
     
